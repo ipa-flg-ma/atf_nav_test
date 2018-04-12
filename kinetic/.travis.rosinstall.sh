2c1d92a7cebaa7089516b90fdb4b3b0989197674
Marcel Albus <marcel.albus@ipa.fraunhofer.de>
2018-04-12 14:09:32 +0200

    [CI] travis test

--- kinetic/.travis.rosinstall.sh
+++ kinetic/.travis.rosinstall.sh
@@ -3,8 +3,8 @@ set -v
 
 while true; do echo "INSTALL IS RUNNING" && sleep 60; done&
 
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list'
-wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt-get update -qq > /dev/null 2>&1
 sudo apt-get install -qq -y python-rosdep python-wstool > /dev/null 2>&1
 sudo apt-get install -y ros-$CI_ROS_DISTRO-ros-base # > /dev/null 2>&1
