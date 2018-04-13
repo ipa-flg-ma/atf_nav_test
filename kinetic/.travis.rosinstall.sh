8cf1887165ab9521d4ffa1ea0000bea2b963df20
Marcel Albus <marcel.albus@ipa.fraunhofer.de>
2018-04-13 12:01:50 +0200

    [CI] travis test

--- kinetic/.travis.rosinstall.sh
+++ kinetic/.travis.rosinstall.sh
@@ -1,6 +1,10 @@
 set -e
 set -v
 
+echo '######################################'
+echo $TRAVIS_BUILD_DIR
+echo '######################################'
+
 while true; do echo "INSTALL IS RUNNING" && sleep 60; done&
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
@@ -11,7 +15,7 @@ sudo apt-get install -qq -y ros-$CI_ROS_DISTRO-ros-base > /dev/null 2>&1
 sudo rosdep init
 rosdep update
 
-source /opt/ros/$CI_ROS_DISTRO/setup.bash # > /dev/null 2>&1 # source release
+source /opt/ros/$CI_ROS_DISTRO/setup.bash > /dev/null 2>&1 # source release
 # create empty ATF workspace
 mkdir -p $ATF_WS_SRC
 
@@ -21,7 +25,6 @@ catkin_make -DCMAKE_BUILD_TYPE=Release # build empty ATF devel space
 catkin_make -DCMAKE_BUILD_TYPE=Release install # build empty ATF install space
 cd $ATF_WS_SRC
 # clone ATF to src/
-#git clone git@github.com:ipa-flg-ma/atf.git
 git clone https://github.com/ipa-flg-ma/atf.git
 cd $ATF_WS
 # populate ATF
@@ -29,7 +32,7 @@ if [ -f src/.travis.rosinstall ]; then wstool init -j10 $ATF_WS_SRC/.travis.rosi
 # install dependencies from ATF
 rosdep install -q --from-paths $ATF_WS_SRC -i -y --rosdistro $CI_ROS_DISTRO > /dev/null #2>&1
 # build devel space of underlay
-source $ATF_WS/devel/setup.bash # > /dev/null 2>&1 # source devel space of underlay
+source $ATF_WS/devel/setup.bash > /dev/null 2>&1 # source devel space of underlay
 catkin_make -DCMAKE_BUILD_TYPE=Release
 # build install space of underlay
 catkin_make -DCMAKE_BUILD_TYPE=Release install # > /dev/null #2>&1
@@ -41,6 +44,11 @@ catkin_init_workspace $CATKIN_WS_UNDERLAY_SRC
 cd $CATKIN_WS_UNDERLAY
 catkin_make -DCMAKE_BUILD_TYPE=Release # build empty underlay devel space
 catkin_make -DCMAKE_BUILD_TYPE=Release install # build empty underlay install space
+
+
+########################### today ##########################
+
+
 # populate underlay
 if [ -f $TRAVIS_BUILD_DIR/.travis.rosinstall ]; then wstool init -j10 src $TRAVIS_BUILD_DIR/.travis.rosinstall; fi
 if [ ! -f $TRAVIS_BUILD_DIR/.travis.rosinstall ]; then wstool init -j10 src $DEFAULT_ROSINSTALL; fi
